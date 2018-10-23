ef08caf4ef27bb130c69baec4fa8f812ad4a18a1
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2018-10-22 21:30:44 -0300

    Uploading Local Code

---
+++ Extrato.java
@@ -0,0 +1,14 @@
+package atm_sotelli;
+
+import java.time.LocalDateTime;
+
+
+public class Extrato {
+
+    String tipo;
+    int valor;
+    LocalDateTime hora;
+    public Extrato() {
+        
+    }
+}
